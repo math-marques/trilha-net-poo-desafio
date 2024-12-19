211b5d43b0edda40934622ba2aac4c32a8f45909
Matheus Marques <146294794+math-marques@users.noreply.github.com>
2024-12-19 15:23:45 -0300

    Update Program.cs

--- trilha-net-poo-desafio/Program.cs
+++ trilha-net-poo-desafio/Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
-
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Nokia meuNokia = new Nokia("12345", "Nokia 3310", 123456789, 16);
+        meuNokia.Ligar();
+        meuNokia.ReceberLigacao();
+        meuNokia.InstalarAplicativo("WhatsApp");
+    }
+}
